cfb23f2ba3bc84f12dc32eba8e8187767e48b129
stringnine <alexjee85@gmail.com>
2026-08-03 19:46:20 +0900

    fix: 메인 홈 사이드바 아이콘 반영 및 폰트 크기 수정 (#71)

--- src/features/onboarding/components/sidebar/Logo.tsx
+++ src/features/onboarding/components/sidebar/Logo.tsx
@@ -1,6 +1,6 @@
 const Logo = () => {
   return (
-    <div className="flex h-14 w-full items-center gap-2.5 border-b border-gray-200 px-5">
+    <div className="flex h-14 w-full items-center gap-2.5 px-5">
       <div className="flex h-6 w-6 items-center justify-center rounded-sm">
         <img
           src="/Wevo-logo.svg"
@@ -8,8 +8,8 @@ const Logo = () => {
           className="h-full w-full object-contain"
         />
       </div>
-      <span className="text-m font-bold tracking-tight text-gray-800">
-        WEVO
+      <span className="text-lg font-bold tracking-tight text-gray-800">
+        Wevo
       </span>
     </div>
   );
